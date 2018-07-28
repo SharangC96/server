beba2051681f0e0cb409d89b7c4c1b2b0f15a4f3
sharang <sharang.chopra115@gmail.com>
2018-07-28 20:22:51 +0530

    first commit

---
+++ src/main/java/com/expedia/server/controller/mainController.java
@@ -0,0 +1,17 @@
+package com.expedia.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainController {
+
+    @RequestMapping("/home")
+    public String home()
+    {
+        System.out.println("hello");
+        return "sharang";
+    }
+
+}
