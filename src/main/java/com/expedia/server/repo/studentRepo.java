21f470dcb1af14ca5f0143b7ad1e3c55b62d3828
sharang <sharang.chopra115@gmail.com>
2018-07-29 15:03:16 +0530

    using jpa instead of curd

--- src/main/java/com/expedia/server/repo/studentRepo.java
+++ src/main/java/com/expedia/server/repo/studentRepo.java
@@ -2,7 +2,7 @@ package com.expedia.server.repo;
 
 import com.expedia.server.models.student;
 import com.sun.xml.internal.bind.v2.model.core.ID;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface studentRepo extends CrudRepository<student,ID> {
+public interface studentRepo extends JpaRepository<student,ID> {
 }
