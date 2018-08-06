911966d55d8ed248045b2d4f083544ef77951fdc
sharang <sharang.chopra115@gmail.com>
2018-08-06 20:49:49 +0530

    deleted some files

--- src/main/java/com/expedia/server/repo/studentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.expedia.server.repo;
-
-import com.expedia.server.models.student;
-import com.sun.xml.internal.bind.v2.model.core.ID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface studentRepo extends JpaRepository<student,ID> {
-}
